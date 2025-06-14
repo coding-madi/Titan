7b184e6f6e5cecc979cb50ea158dad27abef5a47
bforbalaji <bforbalaji@hotmail.com>
2025-06-14 20:09:32 +0530

    chore:  hello world commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, web};
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().route("/", web::get().to(|| async { "Hello, World!" })))
+        .bind("0.0.0.0:8888")?
+        .run()
+        .await
 }
